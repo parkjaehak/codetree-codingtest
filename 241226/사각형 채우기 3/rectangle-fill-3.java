d9342845bcf6b6a2230ffb5b00c072befecc59fa
Park Jaehak <atom8426@gmail.com>
2024-12-26 17:23:22 +0900

    Title: 사각형 채우기 3, Time: 97ms, Memory: 9MB, Status: Passed - CodeTree

--- 241226/사각형 채우기 3/rectangle-fill-3.java
+++ 241226/사각형 채우기 3/rectangle-fill-3.java
@@ -1,32 +1,21 @@
+import java.io.*;
 import java.util.*;
 
 public class Main {
-    static Scanner sc = new Scanner(System.in);
-    static long[]dp;
-    public static final int MOD = 1000000007;
-
-    
-    public static void main(String[] args) {
-        int N = sc.nextInt();    
-        dp = new long[N + 1];
-        
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        long[] dp = new long[N+1];
+        int MOD = (int)(1e9 + 7);
         dp[0] = 1;
         dp[1] = 2;
+        long total = 0;
 
-        for(int i = 2; i <= N; i++){
-            dp[i] = (dp[i-1] * 2 + dp[i-2] * 3) % MOD;
-            for(int j = i -3; j >=0; j--){
-                dp[i] = (dp[i] + dp[j] * 2) % MOD;
-            }
+        for (int i = 2; i <= N; i++) {
+            dp[i] = (dp[i-2] * 3 + dp[i-1] * 2 + 2 * total) % MOD;
+            total = (total + dp[i-2]) % MOD;
         }
+        
         System.out.println(dp[N]);
-
-        //i-1 : 2 X dp[i-1]
-        //i-2: 3 X dp[i-2]
-        //i-3: 2 x dp[i-3]
-        //i-4: 2 x dp[i-4] ...
-        //dp[0] = 1, dp[1] = 2
-        //dp[i] = 2*dp[i-1] + 3*dp[i-2] + 2*(dp[i-3] + dp[i-4] + ... + dp[1] + dp[0])
-
     }
 }
