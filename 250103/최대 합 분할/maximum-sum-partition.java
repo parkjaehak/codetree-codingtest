2dd3cc11396ab80c05fcc66028a69952fe18e1a3
Park Jaehak <atom8426@gmail.com>
2025-01-03 18:22:23 +0900

    Title: 최대 합 분할, Time: 126ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 250103/최대 합 분할/maximum-sum-partition.java
+++ 250103/최대 합 분할/maximum-sum-partition.java
@@ -1,37 +1,43 @@
 import java.util.*;
 
 public class Main {
-    static int n;
-    static int[] arr;
-    static int maxSum = 0;
-
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        n = sc.nextInt();
-        arr = new int[n];
-        for (int i = 0; i < n; i++) {
+        
+        // 입력 받기
+        int n = sc.nextInt();
+        int[] arr = new int[n + 1];
+        int totalSum = 0;
+        
+        for (int i = 1; i <= n; i++) {
             arr[i] = sc.nextInt();
+            totalSum += arr[i];
         }
         
-        //백트래킹을 활용한 분할 탐색
-        backtrack(0, 0, 0, 0);
-        System.out.println(maxSum);
-    }
-    
-    static void backtrack(int index, int sumA, int sumB, int sumC) {
-        if (index == n) {
-            //A와 B의 합이 같다면 최대 합을 갱신
-            if (sumA == sumB) {
-                maxSum = Math.max(maxSum, sumA);
+        // 총합의 절반까지만 고려 (A, B가 동일해야 하므로)
+        int target = totalSum / 2;
+        
+        // DP 배열 초기화 (가능한 합을 저장)  i개의 원소로 sum을 만들 수 있는지 여부를 저장
+        boolean[][] dp = new boolean[n + 1][target + 1];
+        dp[0][0] = true; // 아무것도 선택하지 않은 경우 합 0 가능
+        
+        // DP 진행
+        for (int i = 1; i <= n; i++) {
+            int curr = arr[i];
+            for (int sum = target; sum >= curr; sum--) {
+                dp[i][sum] = dp[i - 1][sum] || dp[i - 1][sum - curr];
             }
-            return;
         }
         
-        // 현재 숫자를 A 그룹에 넣기
-        backtrack(index + 1, sumA + arr[index], sumB, sumC);
-        // 현재 숫자를 B 그룹에 넣기
-        backtrack(index + 1, sumA, sumB + arr[index], sumC);
-        // 현재 숫자를 C 그룹에 넣기
-        backtrack(index + 1, sumA, sumB, sumC + arr[index]);
+        // 가능한 최대 합 X 찾기
+        for (int x = target; x >= 0; x--) {
+            if (dp[n][x]) {
+                System.out.println(x);
+                return;
+            }
+        }
+        
+        // 기본적으로 0 출력 (모든 수를 C에 넣는 경우)
+        System.out.println(0);
     }
 }
