b61d76d279719640c10212e31d3399f765d4378d
Park Jaehak <atom8426@gmail.com>
2025-03-07 21:24:06 +0900

    Title: 숫자가 더 큰 인접한 곳으로 이동, Time: 120ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 250307/숫자가 더 큰 인접한 곳으로 이동/move-to-larger-adjacent-cell.java
@@ -0,0 +1,55 @@
+import java.util.Scanner;
+import java.io.*;
+public class Main {
+
+    static int[] dy = {-1, 1 ,0, 0};
+    static int[] dx = {0 ,0, -1, 1}; //상 하 좌 우
+    static int n;
+    static int [][]grid;
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    static StringBuilder sb = new StringBuilder();
+    static int cy, cx;
+
+    public static void main(String[] args) throws Exception {
+        Scanner sc = new Scanner(System.in);
+        n = sc.nextInt();
+        cy = sc.nextInt();
+        cx = sc.nextInt();
+        grid = new int[n+1][n+1];
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n; j++) {
+                grid[i][j] = sc.nextInt();
+            }
+        }
+        sb.append(grid[cy][cx] + " ");
+        
+        while(true){
+            boolean isExist = simulate();
+            if(!isExist){
+                break;
+            }
+            sb.append(grid[cy][cx] + " ");
+        }
+        
+        bw.write(sb.toString());
+        bw.flush();
+
+    }
+
+    static boolean simulate(){
+
+        for(int i = 0; i < 4; i++){
+           int ny = cy + dy[i];
+           int nx = cx + dx[i];
+
+           if(ny >= n || nx >= n || ny < 0 || nx < 0 ||  grid[cy][cx] >= grid[ny][nx]){
+                continue;
+           }
+           cx = nx;
+           cy = ny;
+           return true;
+            
+        }
+        return false;
+    }
+}
