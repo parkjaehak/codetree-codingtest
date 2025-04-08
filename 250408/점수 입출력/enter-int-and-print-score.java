da667eb9eb380369913f132d1fd1c49a15761425
Park Jaehak <atom8426@gmail.com>
2025-04-08 22:43:05 +0900

    Title: 점수 입출력, Time: 125ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 250408/점수 입출력/enter-int-and-print-score.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+
+        System.out.printf("Your score is %d point.", N);
+    }
+}
