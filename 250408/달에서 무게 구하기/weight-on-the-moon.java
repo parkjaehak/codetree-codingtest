43ebdec7801a312f8ed9f38e0bd870ed7aea4176
Park Jaehak <atom8426@gmail.com>
2025-04-08 22:37:06 +0900

    Title: 달에서 무게 구하기, Time: 92ms, Memory: 9MB, Status: Wrong Answer - Codetree

---
+++ 250408/달에서 무게 구하기/weight-on-the-moon.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        int a = 13;
+        double b = 0.165;
+
+        System.out.print(a + " * " + b + " = " + a*b);
+    }
+}
