84040314ce1e252135c4468be240dfa7797d7ac6
Park Jaehak <atom8426@gmail.com>
2025-04-08 22:49:36 +0900

    Title: 실수 받아 그대로 출력, Time: 123ms, Memory: 11MB, Status: Passed - Codetree

---
+++ 250408/실수 받아 그대로 출력/enter-real-value-and-print.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        double N = sc.nextDouble();
+        // Please write your code here.
+        System.out.printf("%.2f", N);
+    }
+}
