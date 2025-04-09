be6aab15103b0308e00f573ac37edac654a7e431
Park Jaehak <atom8426@gmail.com>
2025-04-09 13:01:24 +0900

    Title: M으로 계속 나누기, Time: 129ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 250409/M으로 계속 나누기/continue-dividing-by-m.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int M = sc.nextInt();
+        
+        // Please write your code here.
+        System.out.println(N);
+        while(true){
+            N /= M;
+            if(N == 0){
+                break;
+            }
+            System.out.println(N);
+        }
+    }
+}
