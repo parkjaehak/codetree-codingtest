0f8adf0c3afcb9a19d7de870e9fd69df1bfb90df
Park Jaehak <atom8426@gmail.com>
2025-04-12 13:52:32 +0900

    Title: 0 나올때까지 출력, Time: 121ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 250412/0 나올때까지 출력/print-until-zero-is-given.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner sc = new Scanner(System.in);
+
+        int n = 0;
+        while(true){
+            n = sc.nextInt();
+            if(n == 0){
+                break;
+            }
+            System.out.println(n);
+        }
+
+
+    }
+}
