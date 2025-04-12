dbf05d629d6ad345d9b73b25caa645e8f4ba9aa9
Park Jaehak <atom8426@gmail.com>
2025-04-12 13:21:42 +0900

    Title: A부터 B까지 짝수의 합, Time: 111ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 250412/A부터 B까지 짝수의 합/sum-of-even-nums-from-a-to-b.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        int result = 0;
+        for(int i = a; i <= b; i++){
+            if(i % 2 == 0){
+                result += i;
+            }
+        }
+        System.out.print(result);
+    }
+}
