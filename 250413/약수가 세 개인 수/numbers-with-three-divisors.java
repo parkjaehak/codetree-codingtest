f053aaf5d3f05ba88b9f6c81bc749f3535f958c7
Park Jaehak <atom8426@gmail.com>
2025-04-13 12:06:33 +0900

    Title: 약수가 세 개인 수, Time: 122ms, Memory: 10MB, Status: Wrong Answer - Codetree

---
+++ 250413/약수가 세 개인 수/numbers-with-three-divisors.java
@@ -0,0 +1,41 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner sc = new Scanner(System.in);
+
+        int start = sc.nextInt();
+        int end = sc.nextInt();
+
+        int count = 0;
+        for(int i = start; i <= end; i++){
+            //약수가 세개인 수를 구하자
+            //제곱수이면서, 그 제곱의 루트가 소수인 수
+            //2, 3, 5
+       
+            int sqrt = (int) Math.sqrt(i);
+
+            if(sqrt * sqrt == i){
+                boolean isPrime = true;
+                if(i < 2){
+                    continue;      
+                }
+                for(int j = 2; j <= sqrt; j++){
+                //2부터 해당 수까지 소수인지 판별
+                    if(sqrt % j == 0){
+                        //나누어 떨어지면 안됨
+                        isPrime = false;
+                        break;
+                    }
+                }
+
+                if(isPrime){
+                    count++;
+                }
+            }
+        
+        }
+
+        System.out.print(count);
+    }
+}
