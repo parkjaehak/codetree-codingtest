6599c78de81d2674e8e78b98eb2f9a56fa3bd95b
Park Jaehak <atom8426@gmail.com>
2025-05-01 00:23:52 +0900

    Title: 줄 세우기 2, Time: 176ms, Memory: 11MB, Status: Passed - Codetree

---
+++ 250501/줄 세우기 2/line-up-students-2.java
@@ -0,0 +1,39 @@
+import java.util.*;
+public class Main {
+     static List<Info> infos = new ArrayList<>();
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        for (int i = 0; i < n; i++) {
+
+            int height = sc.nextInt();
+            int weight = sc.nextInt();
+          infos.add(new Info(height, weight, i + 1));
+        }
+        Collections.sort(infos);
+
+        for(Info info : infos){
+            System.out.println(info.height + " " + info.weight + " " + info.index);
+        }
+    }
+
+
+    static class Info implements Comparable<Info>{
+        int height, weight, index;
+
+
+        Info(int height, int weight, int index){
+            this.height = height;
+            this.weight = weight;
+            this.index = index;
+        }
+
+        @Override
+        public int compareTo(Info info){
+            if(this.height == info.height){
+                return info.weight - this.weight;
+            }
+            return this.height - info.height;
+        }
+    }
+}
