e07adcbb440c644beb50a149f077c748af1ba834
Park Jaehak <atom8426@gmail.com>
2025-05-01 13:57:41 +0900

    Title: 블럭쌓는 명령2, Time: 133ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 250501/블럭쌓는 명령2/block-stacking-commands2.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt(); // 배열 크기
+        int K = sc.nextInt(); // K개의 범위 입력
+
+        int[][] infos = new int[K][2]; // (A, B) 쌍을 저장할 2차원 배열
+
+        for (int i = 0; i < K; i++) {
+            infos[i][0] = sc.nextInt();
+            infos[i][1] = sc.nextInt();
+        }
+
+        int[] array = new int[N + 1]; // N 크기의 배열
+
+        for (int i = 0; i < K; i++) {
+            int start = infos[i][0]; // 시작 인덱스
+            int end = infos[i][1];   // 끝 인덱스
+
+            for (int j = start; j <= end; j++) { 
+                array[j] += 1;
+            }
+        }
+
+        // 최댓값 찾기
+        int maxVal = Integer.MIN_VALUE;
+        for (int i = 1; i <= N; i++) {
+            if (array[i] > maxVal) {
+                maxVal = array[i];
+            }
+        }
+
+        System.out.println(maxVal);
+    }
+}
