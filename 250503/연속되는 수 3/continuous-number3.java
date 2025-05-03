9de2f8a319a65e54da28cf01f2f096cd17e8e70c
Park Jaehak <atom8426@gmail.com>
2025-05-03 21:15:43 +0900

    Title: 연속되는 수 3, Time: 164ms, Memory: 11MB, Status: Passed - Codetree

---
+++ 250503/연속되는 수 3/continuous-number3.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++)
+            arr[i] = sc.nextInt();
+        
+
+        int cnt = 1;
+        int max = 1;
+        for(int i = 1; i < n; i++){
+            if (arr[i] * arr[i - 1] > 0) {
+                cnt++;      // 연속 중이면 증가
+            } else {
+                cnt = 1;    // 연속이 끊기면 초기화
+            }
+
+            max = Math.max(cnt, max);
+        }
+         
+        System.out.print(max);
+    }
+}
