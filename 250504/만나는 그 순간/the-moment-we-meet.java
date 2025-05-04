6510867fbad9c9cfe6b939a057549b385d575f95
Park Jaehak <atom8426@gmail.com>
2025-05-04 16:16:11 +0900

    Title: 만나는 그 순간, Time: 130ms, Memory: 10MB, Status: Wrong Answer - Codetree

---
+++ 250504/만나는 그 순간/the-moment-we-meet.java
@@ -0,0 +1,59 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        int [] a = new int[1000];
+        int [] b = new int[1000]; //매초마다의 위치 기록
+        
+        // Please write your code here.
+
+        int timeA = 0;
+        int posA = 0;
+        for(int i = 0; i < n; i++){
+            char dir = sc.next().charAt(0);
+            int time = sc.nextInt();
+            int move = 0;
+            if(dir == 'R'){
+                move = 1;
+            }else if(dir == 'L'){
+                move = -1;
+            }
+
+            for(int t = 1; t <= time; t++){
+                posA += move;
+                a[timeA++] = posA;
+            }
+        }
+
+        int timeB = 0;
+        int posB = 0;
+        for(int j = 0; j < m; j++){
+          char dir = sc.next().charAt(0);
+            int time = sc.nextInt();
+            int move = 0;
+            if(dir == 'R'){
+                move = 1;
+            }else if(dir == 'L'){
+                move = -1;
+            }
+
+            for(int t = 1; t <= time; t++){
+                posB += move;
+                b[timeB++] = posB;
+            }
+        }
+
+        
+        int maxTime = Math.max(timeA, timeB);
+        for(int i = 1; i <= maxTime; i++){
+            if(a[i] == b[i]){
+                System.out.print(i + 1);
+                return;
+            }
+        }
+        System.out.print(-1);
+    }
+}
