04d0171cf5b7a7c35bd544e1df8351df1baebcfa
Park Jaehak <atom8426@gmail.com>
2025-05-09 16:14:31 +0900

    Title: 일렬로 서있는 소 2, Time: 131ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 250509/일렬로 서있는 소 2/cattle-in-a-rowing-up-2.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++)
+            arr[i] = sc.nextInt();
+        // Please write your code here.
+
+        int count = 0;
+
+        for(int i = 0; i < arr.length; i++){
+            for(int j = i + 1; j < arr.length; j++){
+                for(int k = j + 1; k < arr.length; k++){
+                    if(arr[i] <= arr[j] && arr[j] <= arr[k]){
+                        count++;
+                    }
+                }
+            }
+        }
+
+        System.out.print(count);
+    }
+}
