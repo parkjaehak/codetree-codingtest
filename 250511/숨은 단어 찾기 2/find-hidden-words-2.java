fa2358838ab17d7441ff938cbff7da3d9a6e8d60
Park Jaehak <atom8426@gmail.com>
2025-05-11 21:59:40 +0900

    Title: 숨은 단어 찾기 2, Time: 131ms, Memory: 10MB, Status: Passed - Codetree

--- 250511/숨은 단어 찾기 2/find-hidden-words-2.java
+++ 250511/숨은 단어 찾기 2/find-hidden-words-2.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 public class Main {
-    static int []dy = {-1, -1, 0, 1,1,1,0,-1}; //북동남서 
-    static int []dx = {0,1,1,1,0,-1,-1,-1};
+    static int[] dy = {-1, -1, 0, 1, 1, 1, 0, -1}; // 북, 북동, 동, 남동, 남, 남서, 서, 북서
+    static int[] dx = {0, 1, 1, 1, 0, -1, -1, -1};
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
@@ -13,40 +13,24 @@ public class Main {
                 arr[i][j] = s.charAt(j);
             }
         }
-        
+
         int result = 0;
         for (int i = 0; i < n; i++) {  
-            for(int j = 0; j < m; j++){
-                
-                if(arr[i][j] == 'L'){
-                    for(int k = 0; k < 8; k++){
-                       int y = i;
-                       int x = j;
-                       int count = 0;
-                            
-                        while(true){
-                            int ny = y + dy[k];
-                            int nx = x + dx[k];
-                        
-                            if(ny < 0 || nx < 0 || ny >= n || nx >= m){
-                                break;
-                            }
-                            if('E' != arr[ny][nx]){
-                                break;
-                            }
+            for (int j = 0; j < m; j++) {
+                if (arr[i][j] == 'L') {
+                    for (int k = 0; k < 8; k++) {
+                        int ny1 = i + dy[k];
+                        int nx1 = j + dx[k];
+                        int ny2 = i + 2 * dy[k];
+                        int nx2 = j + 2 * dx[k];
 
-                             count++;
-                            y = ny;
-                            x = nx;
-                        }
-
-                        if(count == 2){
-                            result++;
+                        if (ny2 >= 0 && nx2 >= 0 && ny2 < n && nx2 < m) {
+                            if (arr[ny1][nx1] == 'E' && arr[ny2][nx2] == 'E') {
+                                result++;
+                            }
                         }
                     }
-
                 }
-
             }
         }
 
