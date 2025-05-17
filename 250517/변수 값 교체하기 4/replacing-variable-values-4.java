0a9753162623e1c4a21ce0d9f85c3f4e92e51b41
Park Jaehak <atom8426@gmail.com>
2025-05-17 10:46:39 +0900

    Title: 변수 값 교체하기 4, Time: 92ms, Memory: 9MB, Status: Passed - Codetree

---
+++ 250517/변수 값 교체하기 4/replacing-variable-values-4.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        // 변수 선언
+        int a = 7;
+
+        // 값 변경
+        a = 4;
+
+        // 출력
+        System.out.print(a);
+    }
+}
