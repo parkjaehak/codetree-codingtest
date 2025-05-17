258628fb31c57a74483979e57070fa42551ef04e
Park Jaehak <atom8426@gmail.com>
2025-05-17 10:53:32 +0900

    Title: 문자 변경하기, Time: 89ms, Memory: 9MB, Status: Passed - Codetree

---
+++ 250517/문자 변경하기/change-charater.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        // 변수 선언
+        char a = 'C';
+
+        // 값 변경
+        a = 'T';
+
+        // 출력
+        System.out.print(a);
+    }
+}
