ff1e1fa70a13676ccc005c6d910765328e1b9938
Park Jaehak <atom8426@gmail.com>
2025-05-17 10:44:17 +0900

    Title: 정수 선언하고 뺄셈, Time: 85ms, Memory: 9MB, Status: Passed - Codetree

---
+++ 250517/정수 선언하고 뺄셈/define-numbers-and-substract.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        // 변수 선언
+        int a = 97, b = 13;
+
+        // 출력
+        System.out.print(a + " - " + b + " = " + (a - b));
+    }
+}
