867134f1dbaa151002e69b736700a71bbeac226a
Park Jaehak <atom8426@gmail.com>
2025-05-17 10:45:43 +0900

    Title: 길이 단위 변환하기, Time: 96ms, Memory: 9MB, Status: Passed - Codetree

---
+++ 250517/길이 단위 변환하기/change-length-unit.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        // 변수 선언
+        double a = 9.2;
+	    double b = 1.3;
+
+        // 출력
+        System.out.printf("%.1fft = %.1fcm\n%.1fmi = %.1fcm", a, a*30.48, b, b*160934);
+    }
+}
