5c8d8b5274315fe97b19b90f91dbf2845c2f469d
Park Jaehak <atom8426@gmail.com>
2025-05-17 10:53:59 +0900

    Title: 정수 복사, Time: 84ms, Memory: 9MB, Status: Wrong Answer - Codetree

---
+++ 250517/정수 복사/copy-integer.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        // 변수 선언
+        int a = 3, b = 4;
+
+        // 값 변경
+        a = b;
+
+        // 출력
+        System.out.println(a * b);
+    
+    }
+}
