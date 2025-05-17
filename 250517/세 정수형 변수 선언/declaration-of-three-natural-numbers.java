82f1f4e0ba4c5646208a7b55624d13d7a2a1bc41
Park Jaehak <atom8426@gmail.com>
2025-05-17 10:44:40 +0900

    Title: 세 정수형 변수 선언, Time: 80ms, Memory: 9MB, Status: Passed - Codetree

---
+++ 250517/세 정수형 변수 선언/declaration-of-three-natural-numbers.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        // 변수 선언
+        int a = 7, b = 23, c = 30;
+
+        // 출력
+        System.out.print(a + " + " + b + " = " + c);
+    }
+}
