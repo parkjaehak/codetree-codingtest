a02b1ad3f780e17f5955bd84a421ad65405b5bd0
Park Jaehak <atom8426@gmail.com>
2025-05-17 10:50:04 +0900

    Title: 변수 출력하기 3, Time: 91ms, Memory: 9MB, Status: Passed - Codetree

---
+++ 250517/변수 출력하기 3/outputing-variables-3.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        // 변수 선언
+        int a = 1;
+        int b = 2;
+	    char c = 'C';
+
+        // 출력
+        System.out.print(a + "->" + b + "->"+ c);
+    }
+}
