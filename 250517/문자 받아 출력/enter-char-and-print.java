77c52e501bb5c0d4aab285800b20152366b5e086
Park Jaehak <atom8426@gmail.com>
2025-05-17 11:10:23 +0900

    Title: 문자 받아 출력, Time: 122ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 250517/문자 받아 출력/enter-char-and-print.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main (String args[]) {
+        Scanner sc = new Scanner(System.in);
+
+        // 변수 선언
+        char c;
+
+        // 입력
+        c = sc.next().charAt(0);      // 문자 입력
+
+        // 출력
+        System.out.println(c);
+    }
+}
