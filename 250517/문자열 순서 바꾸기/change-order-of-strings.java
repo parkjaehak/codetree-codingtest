8cbbd65a58f4fe3fe45d80f09a3515a6ec7e628c
Park Jaehak <atom8426@gmail.com>
2025-05-17 11:11:21 +0900

    Title: 문자열 순서 바꾸기, Time: -1ms, Memory: -1MB, Status: Compile Error - Codetree

---
+++ 250517/문자열 순서 바꾸기/change-order-of-strings.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main (String args[]) {
+        Scanner sc = new Scanner(System.in);
+
+        // 변수 선언
+        String S, T;
+     
+
+        // 입력
+        S = sc.next();
+        T = sc.next();
+
+
+        // 출력
+        System.out.println(T);
+        System.out.printf("%.2f\n%.2f", b, c);
+    }
+}
