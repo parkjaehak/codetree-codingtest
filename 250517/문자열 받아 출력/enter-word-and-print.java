b911e4d27597467997eafe4bfffdb1ebec95f291
Park Jaehak <atom8426@gmail.com>
2025-05-17 11:10:38 +0900

    Title: 문자열 받아 출력, Time: 122ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 250517/문자열 받아 출력/enter-word-and-print.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main (String args[]) {
+        Scanner sc = new Scanner(System.in);
+
+        // 변수 선언
+        String s;
+
+        // 입력
+        s = sc.next();
+        
+        // 출력
+        System.out.println(s);
+    }
+}
