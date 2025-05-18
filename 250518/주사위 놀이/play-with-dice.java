86f5efb3746115c9ac409f58ae3022a4a9c68591
Park Jaehak <atom8426@gmail.com>
2025-05-18 10:46:46 +0900

    Title: 주사위 놀이, Time: 114ms, Memory: 10MB, Status: Passed - Codetree

--- 250518/주사위 놀이/play-with-dice.java
+++ 250518/주사위 놀이/play-with-dice.java
@@ -4,20 +4,19 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        // 원소 수 n 입력받기
-        int n = sc.nextInt();
-        int[] countArr = new int[10];
-        int[] arr = new int[100];
+        // 변수 선언
+        int[] countArr = new int[7];
+        int[] arr = new int[10];
         
         // arr[i]를 입력받은 후 카운팅 배열을 통해 각각의 빈도 저장
-        for(int i = 0; i < n; i++) {
+        for(int i = 0; i < 10; i++) {
             arr[i] = sc.nextInt();
             countArr[arr[i]]++;
         }
-        
-        // 1부터 9까지 나온 횟수를 출력
-        for(int i = 1; i <= 9; i++) {
-            System.out.println(countArr[i]);
+
+        // 1부터 6까지 나온 횟수를 출력
+        for(int i = 1; i <= 6; i++) {
+            System.out.println(i + " - " + countArr[i]);
         }
     }
 }
