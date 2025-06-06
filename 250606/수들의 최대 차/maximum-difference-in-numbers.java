b103dafb9cdcc84001403489099486aafd6a47f0
Park Jaehak <atom8426@gmail.com>
2025-06-06 19:42:31 +0900

    Title: 수들의 최대 차, Time: 275ms, Memory: 13MB, Status: Passed - Codetree

--- 250606/수들의 최대 차/maximum-difference-in-numbers.java
+++ 250606/수들의 최대 차/maximum-difference-in-numbers.java
@@ -1,42 +1,35 @@
 import java.util.Scanner;
-
 public class Main {
-    public static final int MAX_N = 1000;
-    public static final int MAX_NUM = 10000;
-
-    public static int n, k;
-    public static int[] arr = new int[MAX_N];
-
-    // 구간 [l, r] 
-    // 사이에 들어있는 숫자 개수를 반환합니다.
-    public static int countNum(int l, int r) {
-        int cnt = 0;
-        for(int i = 0; i < n; i++)
-            if(l <= arr[i] && arr[i] <= r)
-                cnt++;
-
-        return cnt;
-    }
-
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-
-        // 입력
-        n = sc.nextInt();
-        k = sc.nextInt();
-        for(int i = 0; i < n; i++)
+        int n = sc.nextInt();
+        int k = sc.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
             arr[i] = sc.nextInt();
+        }
+        // Please write your code here.
 
-        int ans = 0;
-        // 크기가 K인 모든 구간을 잡아
-        // 해당 구간 안에 들어오는 숫자의 개수를 세서
-        // 그 중 최댓값을 계산합니다.
-        for(int i = 1; i <= MAX_NUM; i++) {
-            // 구간 [i, i + k] 사이에 들어있는 숫자를 세어
-            // 최댓값을 계산합니다.
-            ans = Math.max(ans, countNum(i, i + k));
+        //뽑을 수 있는 최대 숫자의 개수
+        //최대 - 최소 는 k를 넘지 않아야한다.
+        //최대 최소의 차가 k를 넘지 않는 모든 경우를 고려한다.
+
+        int max = 0;
+
+        for(int i = 1; i <= 10000; i++){
+            //k를 넘지 않는다면 이제 이 사이에 존재하는 모든 원소를 구한다.
+            int count = 0;
+            for(int l = 0; l < n; l++){
+                if(i + k >= arr[l] && i <= arr[l]){
+                    //이 사이에 있다면 
+                    count++;
+                }
+            }
+
+            max = Math.max(count, max);
+            
         }
 
-        System.out.println(ans);
+        System.out.print(max);
     }
 }
