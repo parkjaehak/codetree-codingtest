0ff8385319a78633a547cb412fa7b9aa4e0b3944
Park Jaehak <atom8426@gmail.com>
2025-06-06 15:54:37 +0900

    Title: 독서실의 거리두기 5, Time: 133ms, Memory: 10MB, Status: Passed - Codetree

--- 250606/독서실의 거리두기 5/study-cafe-keeping-distance-5.java
+++ 250606/독서실의 거리두기 5/study-cafe-keeping-distance-5.java
@@ -3,41 +3,36 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        String seat = sc.next();
+        char[] seat = sc.next().toCharArray();
         // Please write your code here.
 
         //최대한 거리를 두자
         //한 명을 더 집어넣었을때 가장 가까운 두 사람 간 거리가 최대가 되도록 하자
+        //새로 앉힌 사람 기준으로 가장 가까운 두 사람 간 거리를 구하는게 아니고 그냥 사람을 앉히고 모든 사람 중 가장 가까운 두 사람간 거리를 최대로 하는 곳을 찾아야한다
 
         //0인칸 모든 칸을 선택해보고
         int max = 0;
         for(int i = 0; i < n; i++){
-            int left = Integer.MAX_VALUE;
-            int right = Integer.MAX_VALUE;
-
-            if(seat.charAt(i) == '1'){
+      
+            if(seat[i] == '1'){
                 continue;
             }
 
-            //해당칸에서 거리를 측정하고
-            //왼쪽거리
-            for(int l = i - 1; l >= 0; l--){
-                if(seat.charAt(l) == '1'){
-                    left = i - l; //현재위치에서 최초1인위치를 뺀다
-                    break;
-                }
-
-            }
-            //오른쪽거리
-            for(int r = i + 1; r < n; r++){
-                if(seat.charAt(r) == '1'){
-                    right = r - i;
-                    break;
+            //가장 가까운 사람간의 거리를 구한다.
+            seat[i] = '1';
+            int min = n;
+            for(int j = 0; j < n; j++){
+                for(int k = j + 1; k < n; k++){
+                    //모든 쌍을 조사한다.
+                    if(seat[k] == '1' && seat[j] == '1'){
+                        //가장 가까운 사람을 구한다.
+                        min = Math.min(min, k - j);
+                    }
                 }
             }
 
-            int min = Math.min(left, right);
             max = Math.max(min, max);
+            seat[i] = '0';
 
         }
 
