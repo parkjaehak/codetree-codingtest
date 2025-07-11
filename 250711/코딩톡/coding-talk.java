9d6a962ccec4adfc4d24bdc923339f13d2b3d693
Park Jaehak <atom8426@gmail.com>
2025-07-11 18:22:00 +0900

    Title: 코딩톡, Time: 128ms, Memory: 10MB, Status: Wrong Answer - Codetree

--- 250711/코딩톡/coding-talk.java
+++ 250711/코딩톡/coding-talk.java
@@ -14,23 +14,18 @@ public class Main {
             u[i] = sc.nextInt(); //최종적으로 읽지 않은 사람 수 
         }
    
-     
-        boolean[] people = new boolean[N + 1];
-        //특정 사람의 이름과 가능성을 함께 묶어서 관리
-
-        for(int i = p; i < M; i++){
-            //메세지를 보낸 사람을 찾고 해당 사람은 pass
-            char name = c[i];
-            people[name - 'A' + 1] = true; //pass
+        //각 사람별로 마지막으로 읽은 메시지 번호를 추적
+        int[] lastRead = new int[N]; // 각 사람의 마지막으로 읽은 메시지 번호
+        
+        for (int i = 0; i < M; i++) {
+            int senderIdx = c[i] - 'A';
+            lastRead[senderIdx] = i + 1; // i번째 메시지까지 읽음
+         // 추가로 읽지 않은 사람 수(u[i])를 활용해 상태를 추적해야 함
         }
-        //내가 메세지를 보냈기 때문에 나도 pass
-        char myName = c[p - 1];
-        people[myName - 'A' + 1] = true;
 
-
-        for (int j = 1; j <= N; j++) {
-            if (!people[j]) {
-                char name = (char) ('A' + j - 1);
+        for (int i = 0; i < N; i++) {
+            if (lastRead[i] < p) {
+                char name = (char)('A' + i);
                 System.out.print(name + " ");
             }
         }
