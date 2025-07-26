41c04b216ae17c5d53201a0749df4549fa43c51b
Park Jaehak <atom8426@gmail.com>
2025-07-26 17:16:57 +0900

    Title: 계단 오르기, Time: 125ms, Memory: 10MB, Status: Passed - Codetree

--- 250726/계단 오르기/climbing-stairs.java
+++ 250726/계단 오르기/climbing-stairs.java
@@ -1,25 +1,19 @@
 import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        // Please write your code here.
+        int n = sc.nextInt(); // 2 <= n <= 1000
 
-        //두 계단 혹은 세 계단만 이동가능
+        int[] dp = new int[1001];
 
-        int []dp = new int[1001];
-
-
-        dp[0] = 0;
-        dp[1] = 0;
         dp[2] = 1;
         dp[3] = 1;
 
-        for(int i = 4; i <= n; i++){
-            dp[i] = dp[i - 2] + dp[i - 3];
+        for (int i = 4; i <= n; i++) {
+            dp[i] = (dp[i - 2] + dp[i - 3]) % 10007;
         }
 
-        System.out.print(dp[n] % 10007);
-
+        System.out.print(dp[n]);
     }
 }
