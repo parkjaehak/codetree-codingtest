da9397ba450806cddd6ea39eecb8ac34a461005c
Park Jaehak <atom8426@gmail.com>
2025-07-26 16:51:33 +0900

    Title: 피보나치 수, Time: 127ms, Memory: 10MB, Status: Runtime Error - Codetree

---
+++ 250726/피보나치 수/fibonacci-number.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        // Please write your code here.
+
+        int []dp = new int[n];
+        dp[0] = 1;
+        dp[1] = 1;
+
+        for(int i = 2; i < n; i++){
+            dp[i] = dp[i - 1] + dp[i-2];
+        }
+
+        System.out.print(dp[n - 1]);
+    }
+}
